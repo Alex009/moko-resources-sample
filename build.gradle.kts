39c67610b37052aee8651679e6b9808017b26662
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2022-08-22 21:29:44 +0700

    fix kswift error

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
         classpath("com.android.tools.build:gradle:7.2.1")
         classpath("dev.icerock.moko:resources-generator:0.20.1")
-        classpath("dev.icerock.moko:kswift-gradle-plugin:0.5.0")
+        classpath("dev.icerock.moko:kswift-gradle-plugin:0.6.0")
     }
 }
 
